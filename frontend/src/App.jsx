58d436eecebf4832742e0a7b2a404e9bb96334d1
Ashish Vaidya <ashuvaidya2003@gmail.com>
2025-02-04 22:49:32 +0530

    Refactor TypeScript configuration and update Chakra UI usage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,14 @@
-import { Button } from "@chakra-ui/react";
-import { ColorModeButton } from "./components/ui/color-mode";
+import { Box, Button } from "@chakra-ui/react"
+import { ColorModeButton } from "./components/ui/color-mode"
+
 
 function App() {
   return (
-    <>
-    <ColorModeButton/>
-    <Button colorPalette='teal' variant="outline" size="2xl" rounded='xl'>
-      Ghost
-    </Button>
-    </>
-  );
+    <Box>
+      <Button m={5} colorPalette="blue" variant='ghost'>Button</Button>
+      <ColorModeButton />
+    </Box>
+  )
 }
 
-export default App;
-
+export default App
--- frontend/src/components/ui/provider.jsx
+++ frontend/src/components/ui/provider.jsx
@@ -1,22 +1,11 @@
 'use client'
 
-import { ChakraProvider, createSystem, defineConfig} from '@chakra-ui/react'
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 import { ColorModeProvider } from './color-mode'
-
-const config = defineConfig({
-  theme: {
-    tokens:{
-      colors:{
-        primary: 'purple',
-      }
-    }
-  }
-})
-const system = createSystem(config)
 
 export function Provider(props) {
   return (
-    <ChakraProvider value={system}>
+    <ChakraProvider value={defaultSystem}>
       <ColorModeProvider {...props} />
     </ChakraProvider>
   )
