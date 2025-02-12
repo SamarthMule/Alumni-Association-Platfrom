8c26790a69e6b8e9b6df230cd606bf4de50cd601
Dhanraj <dhanrajt003@gmail.com>
2025-02-12 16:34:10 +0530

    otp verification page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router";
 import RegisterPage from "./pages/RegisterPage";
 import StudentProfile from "./pages/StudentProfile";
 import Home from "./pages/Home";
+import OtpVerificationPage from "./pages/Otp";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/studentprofile" element={<StudentProfile />} />
+          <Route path="/otp" element={<OtpVerificationPage/>}/>
 
           <Route path="*" element={<h1>Not Found</h1>} />
         </Route>
